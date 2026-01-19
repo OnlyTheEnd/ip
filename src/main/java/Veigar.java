cf2e2eadd89bd2239e9643296b8f904dbe13013c
lyon <limzelyon@gmail.com>
2026-01-19 23:26:09 +0800

    Level-0
    Added Introduction, exit line, and possible enum for commands

---
+++ src/main/java/Veigar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Veigar {
+    enum COMMAND {
+        bye,
+        list
+    }
+
+
+    public static void main(String[] args){
+        //introduction
+        System.out.println("I am VeigarBot \nYour commands tire me.");
+        //exit
+        System.out.println("I shall return, better and BIGGER!!!");
+
+    }
+}
