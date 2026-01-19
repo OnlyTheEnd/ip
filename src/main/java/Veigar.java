819c9cfcfbba89d02b8deca8495f884557a37803
lyon <limzelyon@gmail.com>
2026-01-19 23:42:42 +0800

    Implemented echo for only commands
    added echoes

--- src/main/java/Veigar.java
+++ src/main/java/Veigar.java
@@ -1,16 +1,38 @@
 import java.util.Scanner;
 public class Veigar {
+    static boolean active = true;
     enum COMMAND {
-        bye,
-        list
+        BYE,
+        LIST,
+        BLAH
     }
+    public static void texts(COMMAND command){
+        System.out.println(command.name().toLowerCase());
+        switch (command) {
+            case BYE:
+                //exit
+                System.out.println("I shall return, better and BIGGER!!!");
+                active = false;
+                break;
+            case BLAH:
 
+            case LIST:
+                return;
+        }
+
+    }
 
     public static void main(String[] args){
         //introduction
         System.out.println("I am VeigarBot \nYour commands tire me.");
-        //exit
-        System.out.println("I shall return, better and BIGGER!!!");
 
+        Scanner scanner = new Scanner(System.in);
+        while (active) {
+            COMMAND c = COMMAND.valueOf(scanner.nextLine().toUpperCase());
+            texts(c);
+
+        }
     }
 }
+
+
