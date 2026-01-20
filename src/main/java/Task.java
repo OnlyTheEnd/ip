e4e3769790271a72694a9ffcfe1752612c9856b4
lyon <limzelyon@gmail.com>
2026-01-20 14:43:03 +0800

    Level 3
    Added Task methods, toString overide
    Implemented mark and unmark in Agent

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean done;
+
+    public Task(String description){
+        this.description = description;
+        this.done = false;
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+    public void markUndone() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        if (this.done) {
+            return "[X]" + this.description;
+        } else {
+            return "[ ]" + this.description;
+        }
+
+
+
+    }
+}
+
--- src/main/java/Veigar.java
+++ src/main/java/Veigar.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 public class Veigar {
-    static List<String> storage = new ArrayList<String>();
+    static List<Task> storage = new ArrayList<Task>();
     static boolean active = true;
     enum COMMAND {
         BYE,
         LIST,
-        BLAH
+        BLAH,
+        MARK,
+        UNMARK
     }
 
-    public static void texts(COMMAND command){
+    public static void texts(COMMAND command, String[] whole){
         //System.out.println(command.name().toLowerCase());
         switch (command) {
             case BYE:
@@ -20,10 +22,28 @@ public class Veigar {
                 break;
             case BLAH:
 
-            case LIST:
-                for (int i = 0; i < storage.size(); i ++) {
-                    System.out.println((i+ 1) + ". " + storage.get(i));
+            case LIST: {
+                System.out.println("Suffering awaits!");
+                for (int i = 0; i < storage.size(); i++) {
+                    System.out.println((i + 1) + ". " + storage.get(i).toString());
                 }
+                break;
+            }
+            case MARK: {
+                int n = Integer.parseInt(whole[1]);
+                System.out.println("Your commands tire me.\n");
+                storage.get(n-1).markDone();
+                System.out.println(storage.get(n).toString());
+                break;
+            }
+
+            case UNMARK: {
+                int n = Integer.parseInt(whole[1]);
+                System.out.println("No, no, NO! THIS ISN'T OVER!!!");
+                storage.get(n-1).markUndone();
+                System.out.println(storage.get(n).toString());
+                break;
+            }
         }
 
     }
@@ -42,17 +62,18 @@ public class Veigar {
 
     public static void main(String[] args){
         //introduction
-        System.out.println("I am VeigarBot \nYour commands tire me.");
+        System.out.println("I am VeigarBot \nHEHEHEHA");
 
         Scanner scanner = new Scanner(System.in);
         while (active) {
             String s = scanner.nextLine();
-            if (isCommand(s.toUpperCase())) {
-                COMMAND c = COMMAND.valueOf(s.toUpperCase());
-                texts(c);
+            String[] ssplit = s.split(" ");
+            if (isCommand(ssplit[0].toUpperCase())) {
+                COMMAND c = COMMAND.valueOf(ssplit[0].toUpperCase());
+                texts(c, ssplit);
             }
             else {
-                storage.add(s);
+                storage.add(new Task(s));
                 System.out.println("added: " + s);
             }
         }
