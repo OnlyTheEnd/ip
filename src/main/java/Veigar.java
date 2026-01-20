726da7c7bfbdcef6f4f4a5247dc34c2d98bda920
lyon <limzelyon@gmail.com>
2026-01-20 15:40:22 +0800

    Level 4
    Added Task inheritance for toDo, Deadline and Event

--- src/main/java/Veigar.java
+++ src/main/java/Veigar.java
@@ -9,10 +9,13 @@ public class Veigar {
         LIST,
         BLAH,
         MARK,
-        UNMARK
+        UNMARK,
+        TODO,
+        DEADLINE,
+        EVENT
     }
 
-    public static void texts(COMMAND command, String[] whole){
+    public static void texts(COMMAND command, String whole){
         //System.out.println(command.name().toLowerCase());
         switch (command) {
             case BYE:
@@ -24,26 +27,50 @@ public class Veigar {
 
             case LIST: {
                 System.out.println("Suffering awaits!");
+                if (storage.isEmpty()) {
+                    System.out.println("DON'T DEAD OPEN INSIDE");
+                    break;
+                }
                 for (int i = 0; i < storage.size(); i++) {
                     System.out.println((i + 1) + ". " + storage.get(i).toString());
                 }
                 break;
             }
             case MARK: {
-                int n = Integer.parseInt(whole[1]);
+                int n = Integer.parseInt(whole);
                 System.out.println("Your commands tire me.\n");
                 storage.get(n-1).markDone();
-                System.out.println(storage.get(n).toString());
+                System.out.println(storage.get(n-1).toString());
                 break;
             }
 
             case UNMARK: {
-                int n = Integer.parseInt(whole[1]);
+                int n = Integer.parseInt(whole);
                 System.out.println("No, no, NO! THIS ISN'T OVER!!!");
                 storage.get(n-1).markUndone();
-                System.out.println(storage.get(n).toString());
+                System.out.println(storage.get(n-1).toString());
                 break;
             }
+
+            case TODO: {
+                storage.add(new ToDo(whole));
+                break;
+            }
+
+            case EVENT: {
+                String[] parts = whole.split(" /from | /to ");
+
+                storage.add(new Event(parts[0], parts[1], parts[2]));
+                break;
+            }
+            case DEADLINE: {
+                String[] parts = whole.split("/by");
+                storage.add(new Deadline(parts[0], parts[1]));
+                break;
+            }
+        }
+        if (command == COMMAND.TODO || command == COMMAND.EVENT || command == COMMAND.DEADLINE) {
+            System.out.println("Now you have " + storage.size() + " tasks in the list");
         }
 
     }
@@ -67,13 +94,14 @@ public class Veigar {
         Scanner scanner = new Scanner(System.in);
         while (active) {
             String s = scanner.nextLine();
-            String[] ssplit = s.split(" ");
+            String[] ssplit = s.split(" ", 2);
+            String commandArgs = ssplit.length > 1 ? ssplit[1] : "";
             if (isCommand(ssplit[0].toUpperCase())) {
                 COMMAND c = COMMAND.valueOf(ssplit[0].toUpperCase());
-                texts(c, ssplit);
+                texts(c, commandArgs);
             }
             else {
-                storage.add(new Task(s));
+
                 System.out.println("added: " + s);
             }
         }
